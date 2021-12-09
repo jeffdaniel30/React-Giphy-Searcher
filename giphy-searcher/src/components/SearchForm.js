b13945e8d00de5f5e810e1240e634d6886ad0723
jeffdaniel30 <jeffdaniel30@gmail.com>
2021-12-09 14:30:00 -0500

    complete functionality

---
+++ giphy-searcher/src/components/SearchForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { SearchIcon } from '.././shared/AppIcon'
+function SearchForm({ handleSubmit, handleChange, searchString }) {
+    return (
+      <form onSubmit={handleSubmit} className="form-horizontal">
+        <input
+          placeholder="Search"
+          type="text"
+          name="searchString"
+          required
+          onChange={handleChange}
+          value={searchString}
+        />
+        <button type="submit"><SearchIcon height="2rem" width="2rem" /></button>
+      </form>
+    );
+  }
+export default SearchForm
